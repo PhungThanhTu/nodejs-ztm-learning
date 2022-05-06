b00bff94fa31590f6ea1bf59701c7c11fb662d12
phungthanhtu <19522455@gm.uit.edu.vn>
2022-05-07 03:01:51 +0700

    asynchronous

---
+++ first-app/race.js
@@ -0,0 +1,3 @@
+// setInterval(() => console.log('🐇 finishes'),1000);
+// console.log(JSON.stringify({food:'love'}));
+// console.log('🐢 finishes');
--- first-app/server.js
+++
@@ -1,9 +0,0 @@
-const mission = process.argv[2];
-
-if(mission === "learn") {
-    console.log("time to write some Node app");
-}
-else {
-    console.log(`Is ${mission} really more fun ?`);
-}
-console.log(process.argv);
