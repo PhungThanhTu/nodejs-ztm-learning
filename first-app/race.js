ed08be3050995666aea6e89eb7739d5932259615
phungthanhtu <19522455@gm.uit.edu.vn>
2022-05-08 00:37:50 +0700

    Update race.js

--- first-app/race.js
+++ first-app/race.js
@@ -1,3 +1,28 @@
-// setInterval(() => console.log('🐇 finishes'),1000);
-// console.log(JSON.stringify({food:'love'}));
-// console.log('🐢 finishes');
+// Event emitter
+const EventEmitter = require('events');
+
+const celebrity = new EventEmitter();
+
+celebrity.on('race',function(result){
+    if(result === "won")
+    {
+        console.log("Congratulation, you are the best!");
+    }
+
+    
+    }
+);
+
+celebrity.on('race',function(result){
+    if(result === 'won')
+    {
+        console.log("From random with love");
+    }
+}
+);
+
+process.on('exit',(code) => console.log("Program exit with code: " + code));
+
+celebrity.emit('race','lost');
+
+
