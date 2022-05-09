4236236a55ffddda035a8651e3f590e773b0898b
phungthanhtu <19522455@gm.uit.edu.vn>
2022-05-09 09:20:12 +0700

    Update server.js

--- first-app/server.js
+++ first-app/server.js
@@ -1,18 +1,36 @@
-import fetch from 'node-fetch'
-
-function movePlayer(num,pos,callback) {
-    console.log(`moved ${num} steps to ${pos}`);
-    callback();
-}
+// finally
+import fetch from "node-fetch";
 
 const urls = [
     'https://jsonplaceholder.typicode.com/users',
-    'https://jsonpaceholder.typicode.com/posts',
-    'https://jsonplaceholder.typicode.com/albums',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/comments'
 ]
 
-Promise.all(urls.map(url => {
-    return fetch(url).then(resp =>resp.json())
-})).then(result => {
-    console.log(result);
-}).catch(()=> console.log("failed"));
+// Promise.all(urls.map(url => {
+//     return fetch(url).then(people => people.json());
+// })).then(array => {
+//     throw Error;
+//     console.log(array);
+// })
+// .catch(err => console.log(err))
+// .finally(()=> console.log("ahaha"));
+
+
+//for await of
+// instead of await chaining
+const loopThroughUrls = urls => {
+    for(let url of urls){
+        console.log(url)
+    }
+}
+
+const getData2 = async function(urls) {
+    const arrayOfPromises = urls.map(url => fetch(url));
+    for await (let request of arrayOfPromises){
+        const data = await request.json();
+        console.log(data);
+    }
+} 
+
+getData2(urls);
